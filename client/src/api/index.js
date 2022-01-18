f66c8f0d7e1b5d91bd7bff769ec34b15e1129166
Harsh Mule <harsh.mule@extrapreneursindia.com>
2022-01-18 16:02:28 +0530

    chore: umcomment the local URL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const url = axios.create({ baseURL: 'http://localhost:5000'});
-const url = axios.create({ baseURL: 'https://snapshot20.herokuapp.com'});
+const url = axios.create({ baseURL: 'http://localhost:5000'});
+// const url = axios.create({ baseURL: 'https://snapshot20.herokuapp.com'});
 
 
 export const fetchPosts = () => url.get("/posts");
